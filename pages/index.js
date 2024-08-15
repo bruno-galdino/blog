642a68de6e282cf2125d16799d5ff14ca96c851b
Bruno Soares Galdino <bsg.galdino@gmail.com>
2024-08-15 00:20:05 +0000

    Adicionei uma foto a página

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div>
+      <h1>Este é a primeira pedra do meu blog.</h1>
+      <img
+        src="media/img/20240810_165812.jpg"
+        alt="eu"
+        style={{ width: "300px", objectFit: "contain" }}
+      ></img>
+    </div>
+  );
+}
+
+export default Home;
